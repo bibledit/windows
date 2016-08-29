283df19de1635f06d6755051264a92f2f7d95487
Teus Benschop <teusjannette@gmail.com>
2016-08-29 14:13:48 +0200

    Script to remove run-time generated files

---
+++ clean.bat
@@ -0,0 +1,26 @@
+del server\databases\bibleactions.sqlite
+del server\databases\check.sqlite
+del /Q server\databases\config\general\inst*
+del /Q server\databases\config\general\ju*
+del server\databases\confirm.sqlite
+del server\databases\git.sqlite
+del server\databases\jobs.sqlite
+del server\databases\login.sqlite
+del server\databases\mail.sqlite
+del server\databases\mappings.sqlite
+del server\databases\modifications.sqlite
+del server\databases\navigation.sqlite
+del server\databases\noteactions.sqlite
+del server\databases\notes.sqlite
+del server\databases\notes_checksums.sqlite
+del server\databases\privileges.sqlite
+del server\databases\sprint.sqlite
+del server\databases\state.sqlite
+del server\databases\styles.sqlite
+del server\databases\users.sqlite
+del server\databases\versifications.sqlite
+del /Q server\logbook\14*
+del /Q server\processes\14*
+del server\databases\xyz.sqlite
+del server\databases\xyz.sqlite
+del server\databases\xyz.sqlite
