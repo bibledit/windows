a865ef5399ef6c8a7f369d219f725897d047e652
Teus Benschop <teusjannette@gmail.com>
2016-09-13 13:17:28 +0200

    Script for packaging Bibledit in progress...

---
+++ package.bat
@@ -0,0 +1,25 @@
+@echo off
+
+
+echo This will build and package Bibledit
+
+
+echo Setting environment
+setlocal
+SET PATH=%PATH%;C:\Program Files\Git\cmd
+SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
+call vcvarsall.bat x86
+
+
+echo Cloning repository
+rmdir /S /Q C:\bibledit-windows
+cd C:\
+git clone --depth 1 https://github.com/bibledit/bibledit-windows.git
+
+
+echo Building
+cd C:\bibledit-windows
+msbuild
+
+
+pause
