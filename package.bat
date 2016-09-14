1515db54c6df6549eab1e7a6387f7745ba081498
Teus Benschop <teusjannette@gmail.com>
2016-09-14 10:49:31 +0200

    Updated packager

--- package.bat
+++ package.bat
@@ -17,9 +17,16 @@ cd C:\
 git clone --depth 1 https://github.com/bibledit/bibledit-windows.git
 
 
+echo Staging files for packager
+cd C:\
+mkdir bibledit-windows-packager
+xcopy bibledit-windows\server\* bibledit-windows-packager /E /I /Y
+del bibledit-windows-packager\server.*
+
+
 echo Building
 cd C:\bibledit-windows
-msbuild /property:Configuration=Release /property:Platform=x86
+rem msbuild /property:Configuration=Release /property:Platform=x86
 
 
 pause
