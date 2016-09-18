ec124338b2f4c3ce026211354c8a7370ac13bbcc
Teus Benschop <teusjannette@gmail.com>
2016-09-18 17:22:13 +0200

    The packager checks on errors

--- package.bat
+++ package.bat
@@ -14,8 +14,16 @@ SET PATH=%PATH%;C:\Program Files (x86)\Inno Setup 5
 
 echo Cloning repository
 rmdir /S /Q C:\bibledit-windows
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 cd C:\
 git clone --depth 1 https://github.com/bibledit/bibledit-windows.git
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 
 
 echo Staging files for packager
@@ -23,24 +31,52 @@ cd C:\
 rmdir /S /Q C:\bibledit-windows-packager
 mkdir C:\bibledit-windows-packager
 xcopy C:\bibledit-windows\server\* C:\bibledit-windows-packager /E /I /Y
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 del C:\bibledit-windows-packager\server.*
 copy /Y C:\bibledit-windows\package.iss C:\bibledit-windows-packager
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 
 
 echo Building
 cd C:\bibledit-windows
 msbuild /property:Configuration=Release /property:Platform=x86
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 
 
 echo Staging binaries for packager
 copy /Y C:\bibledit-windows\Release\server.exe C:\bibledit-windows-packager
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 copy /Y C:\bibledit-windows\gui\bibledit\bin\Release\bibledit.exe C:\bibledit-windows-packager
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 copy /Y C:\bibledit-windows\vc\*.exe C:\bibledit-windows-packager
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 
 
 echo Creating the Bibledit setup .exe
 cd C:\bibledit-windows-packager
 ISCC package.iss
+if %errorlevel% neq 0 (
+pause
+exit /b %errorlevel%
+)
 
 
 pause
