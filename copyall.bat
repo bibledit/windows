2db6d3ac296672c0a05ce85516e7c6cc4cd77989
Teus Benschop <teusjannette@gmail.com>
2020-12-13 20:02:46 +0100

    Copy build data for testing
    https://github.com/bibledit/cloud/issues/454

---
+++ copyall.bat
@@ -0,0 +1,33 @@
+@echo off
+
+
+echo This will copy all Bibledit files into a staging directory
+
+
+echo Clearing staging directory
+rmdir /S /Q C:\bibledit-windows
+mkdir C:\bibledit-windows
+if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+
+
+echo Copying Bibledit supporting data into staging directory
+xcopy server\* C:\bibledit-windows /E /I /Y /Q
+if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+
+
+echo Cleaning unwanted files from staging directory
+del C:\bibledit-windows\server.*
+
+
+echo Copying kernel binary into staging directory
+copy Debug\server.exe C:\bibledit-windows /Y
+if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+
+
+echo Copying GUI binaries into staging directory
+xcopy gui\bibledit\bin\Release\* C:\bibledit-windows /E /I /Y /Q
+if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+
+
+echo If the script gets here, all went well
+pause
