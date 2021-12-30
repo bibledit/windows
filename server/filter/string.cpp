91350ea25da01016f76b1bc973608a8e36e6b2be
Teus Benschop <teusjannette@gmail.com>
2021-12-30 15:16:16 +0100

    Suppress deprecated C++17 warnings
    https://github.com/bibledit/cloud/issues/719

--- server/filter/string.cpp
+++ server/filter/string.cpp
@@ -17,6 +17,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING 1
+#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING 1
 #include <filter/string.h>
 #include <utf8/utf8.h>
 #include <filter/url.h>
