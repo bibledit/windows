8d78b3215a34105d54e9196e443bed61be99664b
Teus Benschop <teusjannette@gmail.com>
2024-07-16 20:04:52 +0200

    Update script to use mbedtls 2.x for Windows.
    https://github.com/bibledit/cloud/issues/944

--- 1refresh.sh
+++ 1refresh.sh
@@ -90,10 +90,19 @@ sed -i.bak '/HAVE_UTF8PROC/d' config.h
 if [ $? != 0 ]; then exit; fi
 
 
-# Disable threading in mbedTLS on Windows.
-sed -i.bak '/#define MBEDTLS_THREADING_C/d' mbedtls/mbedtls_config.h
+# Since Windows did have a few build problem on mbedTLS 3.x,
+# use mbedTLS 2.x just now.
+rm -rf mbedtls
 if [ $? != 0 ]; then exit; fi
-sed -i.bak '/#define MBEDTLS_THREADING_PTHREAD/d' mbedtls/mbedtls_config.h
+mv mbedtls2windows mbedtls
+if [ $? != 0 ]; then exit; fi
+
+# Disable threading in mbedTLS on Windows.
+# sed -i.bak '/#define MBEDTLS_THREADING_C/d' mbedtls/mbedtls_config.h
+sed -i.bak '/#define MBEDTLS_THREADING_C/d' mbedtls/config.h
+if [ $? != 0 ]; then exit; fi
+# sed -i.bak '/#define MBEDTLS_THREADING_PTHREAD/d' mbedtls/mbedtls_config.h
+sed -i.bak '/#define MBEDTLS_THREADING_PTHREAD/d' mbedtls/config.h
 if [ $? != 0 ]; then exit; fi
 
 
