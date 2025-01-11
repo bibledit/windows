9a2af6284df80396fc5ca7a7197b0371ee2d3722
Teus Benschop <teusjannette@gmail.com>
2025-01-11 14:15:35 +0100

    Fix weird .bat error...
    https://github.com/bibledit/cloud/issues/979

--- 2copyall.bat
+++ 2copyall.bat
@@ -16,12 +16,12 @@ if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
 
 
 echo Copying kernel binary into staging directory
-if exists x64\Release\server.exe (
+if exists "x64\Release\server.exe" (
     echo Copy x64
     copy x64\Release\server.exe C:\bibledit-windows /Y
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
 )
-else if exists ARM64\Release\server.exe (
+else if exists "ARM64\Release\server.exe" (
     echo Copy arm64
     copy ARM64\Release\server.exe C:\bibledit-windows /Y
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
