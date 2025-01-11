77797cd21cc9778a35866adfd9459e67f5f9b7c3
Teus Benschop <teusjannette@gmail.com>
2025-01-11 13:32:09 +0100

    Can also copy server.exe for arm 64
    https://github.com/bibledit/cloud/issues/979

--- 2copyall.bat
+++ 2copyall.bat
@@ -16,8 +16,20 @@ if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
 
 
 echo Copying kernel binary into staging directory
-copy x64\Release\server.exe C:\bibledit-windows /Y
-if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+if exists x64\Release\server.exe (
+    echo Copy x64
+    copy x64\Release\server.exe C:\bibledit-windows /Y
+    if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+)
+else if exists ARM64\Release\server.exe (
+    echo Copy arm64
+    copy ARM64\Release\server.exe C:\bibledit-windows /Y
+    if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
+)
+else (
+     echo Cannot find server.exe
+     exit /b 1
+)
 
 
 echo Copying GUI binaries into staging directory
