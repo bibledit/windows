b707444916c8bfb37256ef2c3a548cdecd620a2a
Teus Benschop <teusjannette@gmail.com>
2025-01-11 20:02:06 +0100

    Detect processor: amd or arm
    https://github.com/bibledit/cloud/issues/979

--- 2copyall.bat
+++ 2copyall.bat
@@ -15,55 +15,53 @@ xcopy server\* C:\bibledit-windows /E /I /Y /Q /EXCLUDE:exclude.txt
 if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
 
 
+echo Getting processor architecture
+set arch=%processor_architecture%
+echo %arch%
+if %arch%=="AMD64" (
+    goto archready
+)
+if %arch%=="ARM64" (
+    goto archready
+)
+echo Unsupported architecture
+exit /b 1
+:archready
+
+
 echo Copying kernel binary into staging directory
 
-if exist "x64\Release\server.exe" (
+if %arch%=="AMD64" (
     echo Copy x64
     copy x64\Release\server.exe C:\bibledit-windows /Y
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
-    goto kernelready
 )
 
-if exist "ARM64\Release\server.exe" (
+if %arch%=="ARM64" (
     echo Copy arm64
     copy ARM64\Release\server.exe C:\bibledit-windows /Y
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
-    goto kernelready
 )
-
-echo Cannot find server.exe
-exit /b 1
-
-:kernelready
 
 
 echo Copying GUI binaries into staging directory
 
-if exist "gui\bibledit\bin\Release\bibledit.exe" (
+if %arch%=="AMD64" (
     echo Copy x64
     xcopy gui\bibledit\bin\Release\* C:\bibledit-windows /E /I /Y /Q
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
-    goto guiready
 )
 
-if exist "gui\bibledit\bin\ARM64\Release\bibledit.exe" (
+if %arch%=="ARM64" (
     echo Copy arm64
     xcopy gui\bibledit\bin\ARM64\Release\* C:\bibledit-windows /E /I /Y /Q
     if %errorlevel% neq 0 ( pause; exit /b %errorlevel% )
-    goto guiready
 )
-
-echo Cannot find bibledit.exe
-exit /b 1
-
-:guiready
 
 
 echo Cleaning unwanted files from staging directory
-if exist "C:\bibledit-windows\Debug" (
+if %arch%=="AMD64" (
     rmdir /S /Q C:\bibledit-windows\Debug
-)
-if exist "C:\bibledit-windows\x64" (
     rmdir /S /Q C:\bibledit-windows\x64
 )
 
